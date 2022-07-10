1b402df5214c73fafc65ae0366e01214c0c83158
victor <913606086@qq.com>
2022-07-10 14:50:02 +0800

    更新部分文件，完成lab4的第三题

--- kernel/trap.c
+++ kernel/trap.c
@@ -66,7 +66,18 @@ usertrap(void)
 
     syscall();
   } else if((which_dev = devintr()) != 0){
-    // ok
+      if(which_dev == 2) {
+        if(p->interval) {
+	  if(p->ticks == p->interval) {
+	    //p->ticks = 0;
+	    //memmove(p->pretrapframe, p->trapframe, sizeof(struct trapframe));
+	    *p->pretrapframe = *p->trapframe;
+	    p->trapframe->epc = p->handler;
+	  }
+	  p->ticks++;
+	}
+        yield();
+      }
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
